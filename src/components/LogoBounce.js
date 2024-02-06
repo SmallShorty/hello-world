1e4d11c82cee4cf1c9c5fdbb3822bd880976ccc0
SmallShorty <46626284+SmallShorty@users.noreply.github.com>
2024-02-06 17:22:42 +0300

    1st bounce effect

--- src/components/LogoBounce.js
+++ src/components/LogoBounce.js
@@ -1,14 +1,61 @@
+import React, { useRef, useState, useEffect } from 'react';
 import './styles/LogoBounce.css';
+
 export const LogoBounce = () => {
+    const logoRef = useRef(null);
+    const bodyRef = useRef(null);
+    const [hRange, setHRange] = useState(0);
+    const [wRange, setWRange] = useState(0);
+    const [positionX, setPositionX] = useState(1);
+    const [positionY, setPositionY] = useState(1);
+
+    useEffect(() => {
+        if (logoRef.current) {
+            const { height, width } = logoRef.current.getBoundingClientRect();
+            setHRange(height);
+            setWRange(width);
+        }
+    }, []);
+
+    useEffect(() => {
+        let animationFrameId;
+
+        let bodyPosition = null;
+        let bodySize = null;
+
+        let logoPosition = null;
+        let logoSize = null;
+
+        const animate = () => {
+            bodyPosition = bodyRef.current.getBoundingClientRect().left;
+            bodySize = bodyRef.current.offsetWidth;
+
+
+
+            setPositionX(positionX => positionX + 1);
+
+            animationFrameId = requestAnimationFrame(animate);
+        };
+
+        animate();
+
+        return () => {
+            cancelAnimationFrame(animationFrameId);
+        };
+    }, [wRange]);
+
+    useEffect(() => {
+        console.log(positionX); // This will log the updated value of positionX
+    }, [positionX]);
+
     return (
         <div className="snippet">
             <div className="header">
-                <span>react</span>
+                <p>react</p>
             </div>
-            <div className="body">
-                <span className="logo">{"<@hello-world/>"}</span>
+            <div className="body" ref={bodyRef}>
+                <span ref={logoRef} className="logo" style={{ position: 'relative', left: positionX, top: positionY }}>{"<@hello-world/>"}</span>
             </div>
-
         </div>
-    )
+    );
 }
