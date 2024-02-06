5276ae8203ab1bc5f9db801e38b0056f519f7ab6
SmallShorty <46626284+SmallShorty@users.noreply.github.com>
2024-02-06 14:32:13 +0300

    added code snippet window, no animation

--- src/components/LogoBounce.js
+++ src/components/LogoBounce.js
@@ -1,7 +1,14 @@
+import './styles/LogoBounce.css';
 export const LogoBounce = () => {
     return (
-        <div>
-            <h1>Hello World</h1>
+        <div className="snippet">
+            <div className="header">
+                <span>react</span>
+            </div>
+            <div className="body">
+                <span className="logo">{"<@hello-world/>"}</span>
+            </div>
+
         </div>
     )
 }
